a41f1206ead63686f11ceb57334d3b93c5abcca7
Ryan Gonzalez <rymg19@gmail.com>
2020-11-01 18:50:45 -0600

    Update to 86.0.4240.111 & remove breakpad

--- update-chromium.sh
+++ update-chromium.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-version=86.0.4240.75
+cp update-chromium.sh /tmp
+git rm -rf .
+cp /tmp/update-chromium.sh .
+
+version=86.0.4240.111
 curl -Lo chromium.tar.xz https://commondatastorage.googleapis.com/chromium-browser-official/chromium-$version.tar.xz
 tar -xvf chromium.tar.xz --strip-components=1
-rm chromium.tar.xz
+sed -i '/angle\|ffmpeg/d' third_party/.gitignore
